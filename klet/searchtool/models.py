668c88b9249c79ebdb9c3aaa8983fada366ec5b6
hrithik <hrithik.b@beehyv.com>
2023-04-04 10:39:26 -0700

    beta

--- klet/searchtool/models.py
+++ klet/searchtool/models.py
@@ -29,4 +29,26 @@ class Record(models.Model):
 
 
 
-# class AddRequest(models.Model):
+class AddRequest(models.Model):
+	GENRE = (('Fictions', 'Fictions'),
+			('Poems', 'Poems'),
+			('Essays', 'Essays'),
+			('Plays', 'Plays'),
+			('Childrens', 'Childrens'),
+			('Classic_General', 'Classic_General'),
+			('Classic_Poem', 'Classic_Poem'),
+			('Classic_History', 'Classic_History'),
+			('Classic_Folk Tale', 'Classic_Folk Tale'),
+			('Classic_Fiction', 'Classic_Fiction'),
+			('Misc','Misc'))
+	authorKorean = models.CharField(max_length=100, null=True, blank=True ,default="")
+	authorEnglish = models.CharField(max_length=100, null=True, blank=True,default="")
+	workTitle = models.CharField(max_length=100, null=True, blank=True,default="")
+	genre= models.CharField(max_length=200, null=True, choices=GENRE, blank=True, default=GENRE[1])
+	translator = models.CharField(max_length=100, null=True, blank=True,default="")
+	sourceTitle = models.CharField(max_length=100, null=True, blank=True,default="")
+	publisher = models.CharField(max_length=100, null=True, blank=True,default="")
+	year = models.CharField(max_length=100,null=True, blank=True, default=" ")
+	yearCreated = models.FloatField(null=True, blank=True, default=00.00)
+	authorEnglish2 = models.CharField(max_length=300, null=True, blank=True, default="")
+	uid2 = models.CharField(max_length=100, default="Not Registered yet")
