9208f04004ff555ae14ddb09a36748523c6eea07
hrithik <hrithik.b@beehyv.com>
2023-05-12 01:43:11 -0700

    12thMay_Dups_SrcTrnT_Genre

--- klet/searchtool/models.py
+++ klet/searchtool/models.py
@@ -4,20 +4,20 @@ from django.db.models import Sum, F, FloatField
 
 
 class Record(models.Model):
-	GENRE = (('Fictions', 'Fictions'),
-			('Poems', 'Poems'),
-			('Essays', 'Essays'),
-			('Plays', 'Plays'),
-			('Childrens', 'Childrens'),
+	GENRE = (('Fiction', 'Fiction'),
+			('Poetry', 'Poetry'),
+			('Essay', 'Essay'),
+			('Play', 'Play'),
+			("Children’s Literature", "Children’s Literature"),
 			('Classic_General', 'Classic_General'),
-			('Classic_Poem', 'Classic_Poem'),
+			('Classic_Poetry', 'Classic_Poetry'),
 			('Classic_History', 'Classic_History'),
 			('Classic_Folk Tale', 'Classic_Folk Tale'),
 			('Classic_Fiction', 'Classic_Fiction'),
 			('Misc','Misc'))
 	authorKorean = models.CharField(max_length=100, null=True, blank=True ,default="")
 	authorEnglish = models.CharField(max_length=100, null=True, blank=True,default="")
-	workTitle = models.CharField(max_length=100, null=True, blank=True,default="")
+	workTitle = models.CharField(max_length=100, null=True, blank=True,default="") # Translation title
 	workTitleKorean = models.CharField(max_length=100, null=True, blank=True,default="")
 	genre= models.CharField(max_length=200, null=True, choices=GENRE, blank=True, default=GENRE[1])
 	translator = models.CharField(max_length=100, null=True, blank=True,default="")
@@ -34,13 +34,13 @@ class AddRequest(models.Model):
 	requestid = models.CharField(max_length=100, null=True, blank=True ,default="")
 	accepted = models.BooleanField(default=False, null=False)
 
-	GENRE = (('Fictions', 'Fictions'),
-			('Poems', 'Poems'),
-			('Essays', 'Essays'),
-			('Plays', 'Plays'),
-			('Childrens', 'Childrens'),
+	GENRE = (('Fiction', 'Fiction'),
+			('Poetry', 'Poetry'),
+			('Essay', 'Essay'),
+			('Play', 'Play'),
+			("Children’s Literature", "Children’s Literature"),
 			('Classic_General', 'Classic_General'),
-			('Classic_Poem', 'Classic_Poem'),
+			('Classic_Poetry', 'Classic_Poetry'),
 			('Classic_History', 'Classic_History'),
 			('Classic_Folk Tale', 'Classic_Folk Tale'),
 			('Classic_Fiction', 'Classic_Fiction'),
